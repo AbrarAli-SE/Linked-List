0778403bc6a5aba828d724538108dd0813703ba4
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-15 18:40:15 +0500

    Practice

--- practice_1.cpp
+++ practice_1.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
+class Node
+{
+    public:
+    int value;
+    Node* next;
+    Node(int val)
+    {
+        value=val;
+        next=NULL;
+
+    }
+};
 int main()
 {
-    cout<<"Hello World";
-    cout<<"sd";
-    cout<<'';
+
+    Node* head=new Node(0);
+
+    cout<<head->value<<" "<<head->next;
+
 }
