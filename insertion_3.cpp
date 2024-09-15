f3e434b7d50c441f8b8a61f10f976ba0e1cec0b1
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-15 23:32:33 +0500

    insertion

---
+++ insertion_3.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+void insertValueAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void insertValueAtTail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void insertValueAtSpacificPoint()
+{
+}
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << " -> " << temp->value;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+
+    Node *head = NULL;
+
+    insertValue(head, 1);
+    display(head);
+
+    insertValue(head, 2);
+    display(head);
+
+    insertValue(head, 3);
+    display(head);
+
+    insertValueAtTail(head, 5);
+    display(head);
+}
