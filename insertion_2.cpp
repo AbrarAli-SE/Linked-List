56a2b8de36b1267fef3c5245239a3a3388aa11ce
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-15 23:15:48 +0500

    Update insertion_2.cpp

--- insertion_2.cpp
+++ insertion_2.cpp
@@ -11,28 +11,46 @@ public:
         next = NULL;
     }
 };
-void insertValue(Node *&head, int val)
+void insertValue(Node* &head, int val)
 {
     Node *newNode = new Node(val);
     newNode->next = head;
     head = newNode;
+}
+void insertValueAtTail(Node* &head, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 void display(Node *head)
 {
     Node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->value << "\n->";
+        cout << " -> "<< temp->value ;
         temp = temp->next;
     }
+    cout<<endl;
 }
 int main()
 {
 
     Node *head = NULL;
-    for (int i = 0; i < 15; i++)
-    {
-        insertValue(head, i);
-    }
+
+    insertValue(head, 1);
     display(head);
-}
+
+    insertValue(head, 2);
+    display(head);
+
+    insertValue(head, 3);
+    display(head);
+
+    insertValueAtTail(head, 5);
+    display(head);
+} 
