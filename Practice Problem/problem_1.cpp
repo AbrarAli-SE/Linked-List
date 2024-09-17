0330814e1f8a467dd307f3f649fb29d33f6c2b36
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-17 19:58:12 +0500

    insertation

---
+++ Practice Problem/problem_1.cpp
@@ -0,0 +1,43 @@
+// 1. 
+//Write a C++ program to create and display a Singly Linked List.
+//Test Data : The list contains the data entered : 11 9 7 5 3 1
+//Solution
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int value;
+    Node* next;
+    Node()
+    {
+        next=NULL;
+    }
+    Node(int val)
+    {
+        value=val;
+        next=NULL;
+    }
+};
+void insert(Node* &head,int val)
+{
+    Node* newNode=new Node(val);
+    newNode->next=head;
+    head=newNode;
+
+}
+void display(Node* &head){
+    Node* temp;
+    while(temp->next!= NULL)
+    {
+        cout<<temp->value<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    Node* head;
+    insert(head,15);
+    display(head);
+}
