5203c3af1bbc367c929d80771265f0e30345fc01
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-17 23:19:29 +0500

    Create problem_2.cpp

---
+++ Practice Problem/problem_2.cpp
@@ -0,0 +1,4 @@
+// Write a C++ program to create a singly linked list of n 
+// nodes and display it in reverse order.
+// Test Data : Original Linked list : 11 9 7 5 3 1 
+// Reverse Linked list : 1 3 5 7 9 11
