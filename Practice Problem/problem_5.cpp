554da57f9848c8f444707cb0cdbde75c1e615033
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-21 01:13:19 +0500

    Update problem_5.cpp

---
+++ Practice Problem/problem_5.cpp
@@ -0,0 +1,10 @@
+// 6. Write a C++ program to find the middle element of a given Linked List.
+// Test Data:
+// Original list:
+// 7 5 3 1
+// Middle element of the said list:
+// 3
+// Original list:
+// 9 7 5 3 1
+// Middle element of the said list:
+// 5
