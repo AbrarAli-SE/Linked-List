2e42c6812a64262b07c974c8c815979f029df36f
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-21 02:04:59 +0500

    Solution

--- Practice Problem/problem_6.cpp
+++ Practice Problem/problem_6.cpp
@@ -1,3 +1,13 @@
+// Write a C++ program to insert a new node at the middle of a given Singly Linked List.
+// Test Data:
+// Original list:
+// 7 5 3 1
+// Singly Linked List: after insert 9 in the middle of the said list-
+// 7 5 9 3 1
+// Singly Linked List: after insert 11 in the middle of the said list-
+// 7 5 9 11 3 1
+// Singly Linked List: after insert 13 in the middle of the said list-
+// 7 5 9 13 11 3 1
 #include <iostream>
 using namespace std;
 class Node
@@ -17,16 +27,6 @@ void insertValueAtHead(Node *&head, int val)
     newNode->next = head;
     head = newNode;
 }
-void insertValueAtTail(Node *&head, int val)
-{
-    Node *newNode = new Node(val);
-    Node *temp = head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-}
 void insertValueAtSpacificPoint(Node *&head, int val, int pos)
 {
     if (pos == 0)
@@ -36,21 +36,34 @@ void insertValueAtSpacificPoint(Node *&head, int val, int pos)
     }
     Node *newNode = new Node(val);
     Node *temp = head;
-    int currentPos = 0;
-    while (currentPos != pos - 1)
+    int currentPosition=0;
+    while(currentPosition!=pos-1)
     {
-        temp = temp->next;
-        currentPos++;
+        temp=temp->next;
+        currentPosition++;
     }
-    newNode->next = temp->next;
-    temp->next = newNode;
+    newNode->next=temp->next;
+    temp->next=newNode;
+}
+int middleIndex(Node *&head)
+{
+    Node *temp = head;
+    int size = 0;
+    while (temp != NULL)
+    {
+        size++;
+        temp = temp->next;
+    }
+    size /= 2;
+    size++;
+    return size;
 }
 void display(Node *head)
 {
     Node *temp = head;
     while (temp != NULL)
     {
-        cout << " -> " << temp->value;
+        cout << " " << temp->value;
         temp = temp->next;
     }
     cout << endl;
@@ -60,17 +73,17 @@ int main()
     Node *head = NULL;
 
     insertValueAtHead(head, 1);
-    display(head);
-
-    insertValueAtHead(head, 2);
-    display(head);
-
     insertValueAtHead(head, 3);
+    insertValueAtHead(head, 5);
+    insertValueAtHead(head, 7);
     display(head);
 
-    insertValueAtTail(head, 5);
+    insertValueAtSpacificPoint(head, 9,middleIndex(head));
     display(head);
 
-    insertValueAtSpacificPoint(head, 7, 3);
+    insertValueAtSpacificPoint(head, 11, middleIndex(head));
+    display(head);
+
+    insertValueAtSpacificPoint(head, 13, middleIndex(head));
     display(head);
 }
