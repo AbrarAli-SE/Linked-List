5fbc0dcce072d534717d645db6e9eb5640abd61a
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-21 01:13:36 +0500

    Update problem_5.cpp

--- Practice Problem/problem_5.cpp
+++ Practice Problem/problem_5.cpp
@@ -8,3 +8,45 @@
 // 9 7 5 3 1
 // Middle element of the said list:
 // 5
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node() {}
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+bool isEmpty(Node *&head)
+{
+    if (head != NULL)
+        return true;
+    return false;
+}
+void displayList(Node *&head)
+{
+    if (isEmpty(head))
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    else
+        cout << "\nEmpty List";
+}
+void insertValueAtStart(Node *&head, int Value)
+{
+    Node *newNode = new Node(Value);
+    newNode->next = head;
+    head = newNode;
+}
+void middleIndexAndValue
