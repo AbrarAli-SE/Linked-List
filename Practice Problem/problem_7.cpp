ee9900013e0aee65b2195380d08122bb12f5f1df
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-21 02:14:29 +0500

    Solution

---
+++ Practice Problem/problem_7.cpp
@@ -0,0 +1,54 @@
+// 8. Write a C++ program to get Nth node in a given Singly Linked List.
+// Test Data:
+// Original list:
+// 7 5 3 1
+// Position: 1
+// Value: 7
+// Position: 2
+// Value: 5
+// Position: 3
+// Value: 3
+// Position: 4
+// Value: 1
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+void insertValueAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void display(Node *head)
+{
+    Node *temp = head;
+    int position=1;
+    while (temp != NULL)
+    {
+        cout << "\nPosition : "<<position<<"\nValue : " << temp->value<<endl;
+        temp = temp->next;
+        position++;
+    }
+
+    cout << endl<<position<<endl;
+}
+int main()
+{
+    Node *head = NULL;
+
+    insertValueAtHead(head, 1);
+    insertValueAtHead(head, 3);
+    insertValueAtHead(head, 5);
+    insertValueAtHead(head, 7);
+    display(head);
+}
