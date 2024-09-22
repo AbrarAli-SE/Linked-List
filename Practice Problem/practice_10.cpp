0c5524b76d26ac541543ad101a0d8f0cebd1e3d7
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-22 18:33:11 +0500

    Create practice_10.cpp

---
+++ Practice Problem/practice_10.cpp
@@ -0,0 +1,73 @@
+// bubble sorting
+//  swaping and sorting
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+void insertValueAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void display(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << " -> " << temp->value;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void sortList(Node *&head)
+{
+    Node *currentNode = NULL;
+    Node *lastSorthedNode=NULL;
+    bool sort=false;
+    do 
+    {
+        nextNode = currentNode->next;
+        while (nextNode != NULL)
+        {
+            if (currentNode->value > nextNode->value)
+            {
+                int temp = currentNode->value;
+                currentNode->value = nextNode->value;
+                nextNode->value = temp;
+            }
+            nextNode = nextNode->next;
+        }
+        currentNode = currentNode->next;
+    }while(currentNode != NULL)
+}
+int main()
+{
+    int size, value;
+    Node *head = NULL;
+    cout << "How much value that you want to store in list : ";
+    cin >> size;
+    for (int i = 1; i <= size; i++)
+    {
+        cout << "Enter value " << i << ": ";
+        cin >> value;
+
+        insertValueAtHead(head, value);
+    }
+    cout << "\nOrignal List:\n";
+    display(head);
+
+    sortList(head);
+
+    cout << "\nUpdate List:\n";
+    display(head);
+}
