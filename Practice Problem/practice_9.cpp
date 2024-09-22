9c018ee306d0460580edee7b4a70b828ee6bc0e4
AbrarAli-SE <abrarali.se@gmail.com>
2024-09-22 15:43:30 +0500

    Create practice_9.cpp

---
+++ Practice Problem/practice_9.cpp
@@ -0,0 +1,49 @@
+//swaping and sorting 
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+    }
+};
+void insertValueAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << " -> " << temp->value;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void sortList()
+{
+    
+}
+int main()
+{
+    int size, value;
+    Node *head = NULL;
+    cout << "How much value that you want to store in list : ";
+    cin >> size;
+    for (int i = 1; i <= size; i++)
+    {
+        cout << "Enter value " << i << ": ";
+        cin >> value;
+
+        insertValueAtHead(head, value);
+    }
+
+}
