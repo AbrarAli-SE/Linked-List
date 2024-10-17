fae4b821e5f353d278fdcf243e816c4a51f6adba
AbrarAli-SE <abrarali.se@gmail.com>
2024-10-18 00:54:13 +0500

    Solution

---
+++ Circular Linked List/Double Circular/deletation/practice_02.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        value = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+class DoubleCircularLinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+
+    DoubleCircularLinkedList()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void insertAtHead(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            tail->next = newNode;
+            head->prev = newNode;
+            return;
+        }
+        newNode->next = head;
+        tail->next = newNode;
+        head->prev = newNode;
+        newNode->prev = tail;
+        head = newNode;
+    }
+    void insertAtTail(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            insertAtHead(val);
+            return;
+        }
+        newNode->next = head;
+        tail->next = newNode;
+        head->prev = newNode;
+        newNode->prev = tail;
+
+        tail = newNode;
+    }
+    void deleteAthead()
+    {
+        tail->next = head->next;
+        head->next->prev = tail;
+        head = head->next;
+    }
+    void deleteAtTail()
+    {
+        tail->prev->next=head;
+        head->prev=tail->prev;
+        tail=tail->prev;
+    }
+    void display()
+    {
+        Node *temp = head;
+        do
+        {
+
+            cout << temp->value << " ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << endl;
+    }
+    void displayReverse()
+    {
+        Node *temp = tail;
+        do
+        {
+
+            cout << temp->value << " ";
+            temp = temp->prev;
+        } while (temp != tail);
+        cout << endl;
+    }
+};
+int main()
+{
+    DoubleCircularLinkedList dcll;
+    dcll.insertAtHead(10);
+    dcll.insertAtHead(20);
+    dcll.insertAtHead(30);
+    dcll.insertAtHead(40);
+    dcll.insertAtHead(50);
+    dcll.insertAtTail(60);
+    dcll.display();
+
+    dcll.deleteAtTail();
+
+    dcll.display();
+    dcll.displayReverse();
+}
