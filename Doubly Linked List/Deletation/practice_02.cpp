bfe0cce8d78b656f1cf0c087e07b93d09486645c
AbrarAli-SE <abrarali.se@gmail.com>
2024-10-17 21:23:36 +0500

    Solution

---
+++ Doubly Linked List/Deletation/practice_02.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    Node *next;
+    Node *prev;
+    int value;
+    Node(int val)
+    {
+
+        value = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+class DoubleLinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+
+    DoubleLinkedList()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void insertNodeAtHead(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+    void deleteAtTail()
+    {
+        tail=tail->prev;
+        tail->next=NULL;
+ 
+    }
+    void deleteAtStart()
+    {
+        Node *temp = head;
+        head = head->next;
+        head->prev = NULL;
+        free(temp);
+    }
+    void display()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    void displayReverse()
+    {
+        Node *temp = tail;
+        while (temp != NULL)
+        {
+
+            cout << temp->value << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+};
+int main()
+{
+    DoubleLinkedList dll;
+    dll.insertNodeAtHead(10);
+    dll.insertNodeAtHead(20);
+    dll.insertNodeAtHead(30);
+    dll.insertNodeAtHead(40);
+    dll.insertNodeAtHead(50);
+    // cout<<dll.tail->value;
+    // dll.display();
+
+    // dll.deleteAtStart();
+    // dll.display();
+
+    dll.deleteAtTail();
+
+    dll.display();
+    dll.displayReverse();
+
+}
